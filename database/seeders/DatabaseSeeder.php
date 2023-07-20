405ea64658394c6feba1123ef7aa75a059ef83dd
JinXu1122 <xu000264@algonquinlive.com>
2023-07-20 15:22:41 -0400

    Database Configuration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Upvote;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'name' => 'Jin Xu',
+            'email' => 'jin@email.com',
+            'password' => Hash::make('password')
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory(9)->create();
+
+        $this->call([
+            IdeaSeeder::class,
+            UpvoteSeeder::class,
+            CommentSeeder::class
+        ]);
     }
 }
